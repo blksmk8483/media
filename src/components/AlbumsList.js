36a47ee8e0739332d3a3a6ef0cfaa999df1a13a3
Brandon Knight <knightbrandon1984@gmail.com>
2023-03-08 09:14:47 -0600

    add album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,10 +1,15 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
 
 function AlbumsList({ user }) {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    }
 
     let content;
     if (isLoading) {
@@ -15,18 +20,25 @@ function AlbumsList({ user }) {
         content = data.map(album => {
             const header = <div>{album.title}</div>
 
-            return <ExpandablePanel key={album.id} header={header}>
-                List of photos in the album
-            </ExpandablePanel>
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of photos in the album
+                </ExpandablePanel>
+            );
         });
     }
 
     return (
         <div>
-    <div>Albums for {user.name}</div>
-    <div>{content}</div>
-    </div>
+            <div>
+                Albums for {user.name}
+            <Button onClick={handleAddAlbum}>
+                + Add Album
+            </Button>
+            </div>
+            <div>{content}</div>
+        </div>
     );
 };
 
-export default AlbumsList
+export default AlbumsList;
